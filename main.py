87f7c7141dada85a33b5b9f11949bb4ee23138c0
Erickson-lopes <ofc.erickson@gmail>
2022-05-08 12:20:49 -0300

    project adamsmith

---
+++ main.py
@@ -0,0 +1,40 @@
+from time import sleep
+import chromedriver_autoinstaller
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def auto_driver() -> webdriver:
+    chromedriver_autoinstaller.install()
+    chrome_options = Options()
+
+    chrome_options.add_argument("-headless")
+    chrome_options.add_argument('--log-level=3')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    return webdriver.Chrome(options=chrome_options)
+
+
+def find_url(id_find: int, driver: webdriver):
+    driver.get(f'https://www.adamsmith.haus/python/examples/{id_find}')
+
+    sleep(1)
+
+    try:
+        driver.find_element(By.CLASS_NAME, "not-found")
+
+    except NoSuchElementException:
+        with open("found_sites.txt", 'a') as file:
+            file.write(driver.current_url + '\n')
+
+
+if __name__ == '__main__':
+    driver_for = auto_driver()
+
+    for num in range(10000):
+        find_url(num, driver_for)
+
+    driver_for.close()
